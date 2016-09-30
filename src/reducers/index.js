edf3cb2ca1bce8f8cf3c2385834b1b8bb8a042bc
Dan Wexler <wex@zorroa.com>
2016-09-30 08:41:23 -0700

    Fully remove react-router-redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
 import { reducer as formReducer } from 'redux-form'
 
 import auth from './authReducer'
 
 export default combineReducers({
   auth,
-  form: formReducer,
-  routing: routerReducer
+  form: formReducer
 })
