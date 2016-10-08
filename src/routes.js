bd8845ba11e5cf063220f1f7af5328785bf05aa0
Dan Wexler <wex@zorroa.com>
2016-10-08 11:58:17 -0700

    Add Welcome to routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,16 @@ import { Route, IndexRoute } from 'react-router'
 
 import App from './containers/App'
 import Signin from './components/auth/Signin'
-import Signup from './components/auth/Signup'
 import Signout from './components/auth/Signout'
 import RequireAuth from './components/auth/RequireAuth'
 import Workspace from './components/Workspace'
+import Welcome from './components/Welcome'
 
 export default (
   <Route path='/' component={App}>
     <IndexRoute component={RequireAuth(Workspace, '/signin')} />
     <Route path="signin" component={Signin} />
-    <Route path="signup" component={Signup} />
     <Route path="signout" component={Signout} />
+    <Route path="welcome" component={Welcome} />
   </Route>
 )
