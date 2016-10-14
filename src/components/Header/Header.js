84912684d0133e52b714a99ae7f341c6d2771865
David Hart <david@zorroa.com>
2016-10-13 22:50:02 -0600

    Center the Header vertically

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,31 +10,33 @@ const Header = () => (
     <div className="flexOn"></div>
     <Searchbar/>
     <div className="flexOn"></div>
-    <ul className="header-menu flexCenter">
-      <li className="header-menu-item">
-        <span>Imports</span>
-        <span className='header-menu-icon icon-arrow-down'></span>
-      </li>
-      <li className="header-menu-item">
-        <span>Layouts</span>
-        <span className='header-menu-icon icon-arrow-down'></span>
-      </li>
-      <li className="header-menu-item">
-        <span>Exports</span>
-        <span className='header-menu-icon icon-arrow-down'></span>
-      </li>
-      <li className="header-menu-item">
-        <span>Admin</span>
-        <span className='header-menu-icon icon-arrow-down'></span>
-      </li>
-      <li className="header-menu-item">
-        <span>Help</span>
-        <span className='header-menu-icon icon-arrow-down'></span>
-      </li>
-      <li className="header-menu-item">
-        <Link className="nav-link" to="/signout">Sign Out</Link>
-      </li>
-    </ul>
+    <div className="fullHeight">
+      <ul className="header-menu flexCenter">
+        <li id="header-menu-imports" className="header-menu-item">
+          <span>Imports</span>
+          <span className='header-menu-icon icon-arrow-down'></span>
+        </li>
+        <li id="header-menu-layouts" className="header-menu-item">
+          <span>Layouts</span>
+          <span className='header-menu-icon icon-arrow-down'></span>
+        </li>
+        <li id="header-menu-exports" className="header-menu-item">
+          <span>Exports</span>
+          <span className='header-menu-icon icon-arrow-down'></span>
+        </li>
+        <li id="header-menu-admin" className="header-menu-item">
+          <span>Admin</span>
+          <span className='header-menu-icon icon-arrow-down'></span>
+        </li>
+        <li id="header-menu-help" className="header-menu-item">
+          <span>Help</span>
+          <span className='header-menu-icon icon-arrow-down'></span>
+        </li>
+        <li id="header-menu-signout" className="header-menu-item">
+          <Link className="nav-link" to="/signout">Sign Out</Link>
+        </li>
+      </ul>
+    </div>
   </nav>
 )
 
