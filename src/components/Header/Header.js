2dc4da252df08b0c59cbaec51582473fc5990603
David Hart <david@zorroa.com>
2016-10-13 22:09:57 -0600

    Header: Simplify layout & add placeholder menu items

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,32 @@ import Logo from '../../components/Logo'
 import Searchbar from '../../components/Searchbar'
 
 const Header = () => (
-  <nav className="header">
-    <div className="header-logo-search">
-      <Link to="/" className="header-logo"><Logo/></Link>
-      <Searchbar/>
-    </div>
-    <ul className="header-menu">
+  <nav className="header flexCenter fullWidth">
+    <Link to="/" className="header-logo"><Logo/></Link>
+    <div className="flexOn"></div>
+    <Searchbar/>
+    <div className="flexOn"></div>
+    <ul className="header-menu flexCenter">
+      <li className="header-menu-item">
+        <span>Imports</span>
+        <span className='header-menu-icon icon-arrow-down'></span>
+      </li>
+      <li className="header-menu-item">
+        <span>Layouts</span>
+        <span className='header-menu-icon icon-arrow-down'></span>
+      </li>
+      <li className="header-menu-item">
+        <span>Exports</span>
+        <span className='header-menu-icon icon-arrow-down'></span>
+      </li>
+      <li className="header-menu-item">
+        <span>Admin</span>
+        <span className='header-menu-icon icon-arrow-down'></span>
+      </li>
+      <li className="header-menu-item">
+        <span>Help</span>
+        <span className='header-menu-icon icon-arrow-down'></span>
+      </li>
       <li className="header-menu-item">
         <Link className="nav-link" to="/signout">Sign Out</Link>
       </li>
