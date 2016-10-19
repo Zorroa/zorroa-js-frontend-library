6b981972b536feb6585505d15b3dfec12aa63bcd
David Hart <david@zorroa.com>
2016-10-19 16:58:12 -0600

    Preserve aspect ratio on assets thumbnails

--- src/components/Thumb/Thumb.js
+++ src/components/Thumb/Thumb.js
@@ -21,26 +21,41 @@ class Thumb extends Component {
     if (!asset.proxies) {
       return <div className="thumb" style={{ backgroundColor: asset.backgroundColor() }} />
     }
-    var width, height
+    var width, height, fit
     switch (layout) {
       case 'grid':
         width = dim
         height = dim
+        fit = 'cover'
         break
       case 'masonry':
       case 'slideshow':
         height = dim
         width = height * asset.aspect()
+        fit = 'contain'
         break
       case 'waterfall':
         width = dim
         height = width / asset.aspect()
+        fit = 'cover'
         break
     }
     const proxy = asset.closestProxy(width, height)
     const selectedStyle = selected ? { border: '3px solid #73b61c' } : { border: '3px solid transparent' }
     return (
+      <img src={proxy.url(host)} width={width} height={height} onDoubleClick={onDoubleClick} />
       <img src={proxy.url(host)} width={width} height={height} style={selectedStyle} onClick={onClick} onDoubleClick={onDoubleClick} />
+      <div
+        className='assets-thumb'
+        style={{
+          'backgroundImage': `url(${proxy.url(host)})`,
+          'backgroundSize': fit,
+          'width': `${width}px`,
+          'height':`${height}px`,
+        }}
+        onDoubleClick={onDoubleClick}
+        onClick={onClick}
+      />
     )
   }
 }
