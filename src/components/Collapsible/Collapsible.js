Zorroa/zorroa-js-frontend-library

commit 7257bcfbdfef60254fb470b42ba65d8d42991498

Collapsible body styles

src/components/Collapsible/Collapsible.js
@@ -10,7 +10,7 @@ export default class Collapsible extends Component {
     return {
       children: PropTypes.node,
       header: PropTypes.element.isRequired,
-      style: PropTypes.object
+      bodyStyle: PropTypes.object
     }
   }
 
@@ -29,14 +29,16 @@ export default class Collapsible extends Component {
 
   render () {
     const { isOpen } = this.state
-    const { children, header, style } = this.props
+    const { children, header, bodyStyle } = this.props
 
     return (
-      <div style={style} className={classnames('collapsible', 'flexCol', {'parent': children, 'open': open})}>
-        <div style={style} className="collapsible-header flexCenter" onClick={this.handleClick.bind(this)}>
+      <div className={classnames('collapsible', 'flexCol', {'parent': children, 'open': open})}>
+        <div className="collapsible-header flexCenter" onClick={this.handleClick.bind(this)}>
           { cloneElement(header, { isOpen, isParent: children && children.length > 0 }) }
         </div>
-        { isOpen && (children) }
+        <div style={bodyStyle} className="collapsible-body">
+          { isOpen && (children) }
+        </div>
       </div>
     )
   }

src/components/CollapsibleHeader/collapsibleheader.scss
@@ -6,7 +6,7 @@
   padding: 5px;
   padding-left: 10px;
   background-color: #d8d8d8;
-  margin-bottom: 5px;
+  margin-top: 5px;
   text-transform: uppercase;
   color: $zorroa-gray;
   font-weight: 100;

src/components/Folders/FolderItem.js
@@ -30,7 +30,7 @@ export default class FolderItem extends Component {
     const { folders, folderId, loadChildren } = this.props
     const folder = folders.get(folderId)
     return (
-      <Collapsible style={{marginLeft: '16px'}} header={this.renderHeader(folder)}>
+      <Collapsible bodyStyle={{marginLeft: '16px'}} header={this.renderHeader(folder)}>
         { folder.children !== undefined && folder.children.map(child => (
           <FolderItem key={child.id} folders={folders} folderId={child.id} loadChildren={loadChildren} />)
         )}

commit 7724a3050625b3cd07df58a6cbb3a82b7536aa85

Fix the SimpleSearch styling
I broke it in commit 7257bcfbdfef60254fb470b42ba65d8d42991498

Signed-off-by: David Hart <david@zorroa.com>

src/components/Collapsible/Collapsible.js
@@ -20,10 +20,10 @@ class Collapsible extends Component {
 
       // props defined by intstantiation
       children: PropTypes.node,
-      style: PropTypes.object,
       header: PropTypes.element.isRequired,
       sidebarIsOpen: PropTypes.bool.isRequired,
-      isOpenKey: PropTypes.string
+      isOpenKey: PropTypes.string,
+      extraClasses: PropTypes.string
     }
   }
 
@@ -52,7 +52,7 @@ class Collapsible extends Component {
   }
 
   render () {
-    const { children, header } = this.props
+    const { children, header, style, extraClasses } = this.props
     const { isOpen } = this.state
 
     const childrenWithProps = _ => {
@@ -63,8 +63,10 @@ class Collapsible extends Component {
         child => cloneElement(child, { sidebarIsOpen }))
     }
 
+
+
     return (
-      <div className={classnames('collapsible', 'flexCol', { 'parent': children, isOpen })}>
+      <div className={classnames('collapsible', 'flexCol', { 'parent': children, isOpen }) + ' ' + extraClasses||''}>
         <div className="collapsible-header flexCenter" onClick={this.handleClick.bind(this)}>
           { cloneElement(header, { isOpen, isParent: children && children.length > 0 }) }
         </div>

src/components/Racetrack/SimpleSearch.js
@@ -38,7 +38,7 @@ class SimpleSearch extends Component {
   }
 
   header () {
-    return <FilterHeader icon="icon-search" label="Simple Search" onClose={this.removeFilter.bind(this)} />
+    return <FilterHeader icon="icon-search" label="Simple Search" onClose={this.removeFilter.bind(this)}/>
   }
 
   // Manage the <input> without a form, using onChange to update local state
@@ -58,18 +58,9 @@ class SimpleSearch extends Component {
   render () {
     const { id, sidebarIsOpen } = this.props;
 
-    const collapsibleStyle = {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'stretch',
-      color: '#ededed',
-      backgroundColor: '#74b618',
-      borderRadius: '3px'
-    }
-
     return (
-      <Collapsible style={collapsibleStyle} header={this.header()} isOpenKey={id} sidebarIsOpen={sidebarIsOpen}>
-        <div className="simple-search">
+      <Collapsible extraClasses={'simple-search'} header={this.header()} isOpenKey={`SimpleSearch|${id}`} sidebarIsOpen={sidebarIsOpen}>
+        <div className="simple-search-body">
           <div>
             <input type="text" placeholder="Search..." value={this.state.queryString}
                    onKeyPress={this.modifySliver} onChange={this.updateQueryString} />

src/components/Racetrack/SimpleSearch.scss
@@ -1,0 +1,33 @@
+@import '../../styles/core-mixins.scss';
+
+.simple-search {
+  & .collapsible-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: stretch;
+    color: $zorroa-nearwhite;
+    background-color: $zorroa-sickly-green;
+    border-radius: 3px 3px 0 0;
+  }
+}
+
+.simple-search-body {
+  background-color: white;
+  color: $zorroa-gray;
+  display: flex;
+  flex-direction: column;
+
+  input {
+    flex: 1;
+    outline: none;
+    background: none;
+    border: 1px solid $zorroa-gray;
+    border-radius: 3px;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 2em;
+    margin: 12px;
+    padding-left: 8px;
+    padding-right: 8px;
+  }
+}

src/components/Racetrack/filter.scss
@@ -15,24 +15,3 @@
     background: none;
   }
 }
-
-.simple-search {
-  background-color: #fff;
-  color: $zorroa-gray;
-  display: flex;
-  flex-direction: column;
-
-  input {
-    flex: 1;
-    outline: none;
-    background: none;
-    border: 1px solid $zorroa-gray;
-    border-radius: 3px;
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 2em;
-    margin: 12px;
-    padding-left: 8px;
-    padding-right: 8px;
-  }
-}

src/components/Racetrack/index.js
@@ -1,4 +1,5 @@
 import Racetrack from './Racetrack'
 import './racetrack.scss'
 import './filter.scss'
+import './SimpleSearch.scss'
 export default Racetrack