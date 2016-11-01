c3e871d2cbec214d3f30a61243a32facaffa5fb4
David Hart <david@zorroa.com>
2016-11-01 16:42:25 -0600

    deploy: add version handling [Finishes #133551925]
    check npm & git version numbers
    create a new branch when not patching
    verify current branch when patching
    create a pull request when not patching
    rearrange funcs to be in same order as called
    bail out if npm version fails

--- build/build-prod.sh
+++ build/build-prod.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Example bump uses
 # patch: sh build-prod.sh patch
@@ -7,11 +7,48 @@
 
 versionType="$1"
 
-bumpVersion() {
-  VERSION=$(npm version $versionType -m "$USER bumped the version to %s for deployment to production on $(date +%Y-%m-%d@%H:%M)")
-  echo "Version bumped to" $VERSION
-  git tag $VERSION -am "Production Distribution Build from $USER on $date"
-  git push origin $VERSION
+CUR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+deployCheck() {
+  # We need the github cli
+  command -v hub >/dev/null 2>&1 || { echo >&2 "Please install hub https://hub.github.com/"; exit 0; }
+
+  # Potential Types: [prepatch, preminor, premajor, prerelease]
+  # Available Types: [patch, minor, major] patch is the default so it is not passed
+  ALLOWED_VERSIONS="patch minor major"
+  if [[ ! " $ALLOWED_VERSIONS " =~ " $versionType " ]]; then
+    echo "Unknown version type \"$versionType\""
+    echo "Known version types: $ALLOWED_VERSIONS"
+    exit 0
+  fi
+
+  GITSTATUS=$(git status -s)
+  GITHASHLOCAL=$(git rev-parse HEAD)
+  GITHASHREMOTE=$(git rev-parse origin/$CUR_BRANCH)
+
+  if [ ! -z "$GITSTATUS" ] || [ "$GITHASHLOCAL" != "$GITHASHREMOTE" ]; then
+    echo "You have uncommitted changes; bailing out."
+    exit 0
+  fi
+
+  # make sure for patches that git branch version and npm version match
+  if [ "$versionType" == "patch" ]; then
+    CUR_NPM_VER=$(npm version | grep zorroa-js-curator | egrep -o '\d+\.\d+\.\d+')
+    CUR_NPM_MAJOR_MINOR=$(echo $CUR_NPM_VER | cut -f -2 -d.)
+    if [ "$CUR_BRANCH" != "$CUR_NPM_MAJOR_MINOR" ]; then
+      echo "You must be in branch $CUR_NPM_MAJOR_MINOR to patch"
+      exit 0
+    fi
+  fi
+
+  if [ "$versionType" != "patch" ] && [ "$CUR_BRANCH" != "master" ]; then
+    echo "Deploying from branch $CUR_BRANCH"
+    read -p "Type $CUR_BRANCH to continue deploying from a custom branch, or Ctrl-C to cancel: " p
+    if [ "$p" != "$CUR_BRANCH" ]; then
+      echo "Name doesn't match; bailing out."
+      exit 0
+    fi
+  fi
 }
 
 build() {
@@ -22,33 +59,39 @@ build() {
   npm run build
 }
 
-deployCheck() {
-  # Potential Types: [prepatch, preminor, premajor, prerelease]
-  # Available Types: [patch, minor, major] patch is the default so it is not passed
-  ALLOWED_VERSIONS="patch minor major"
-  if [[ ! " $ALLOWED_VERSIONS " =~ " $versionType " ]]; then
-    echo "Unknown version type \"$versionType\""
-    echo "Known version types: $ALLOWED_VERSIONS"
+bumpVersion() {
+  # patch? create a new branch
+  if [ "$versionType" != "patch" ]; then
+    git checkout -b ${versionType}_$(date +%s%3N)
+  fi
+
+  # Bump the version number. This creates a commit changing package.json, and a tag with the version number
+  npm version $versionType -m "$USER deployed %s on $(date +%Y-%m-%d@%H:%M)"
+  VERSION_PATCH=$(npm version | grep zorroa-js-curator | egrep -o '\d+\.\d+\.\d+')
+  VERSION_MAJMIN=$(echo $VERSION_PATCH | cut -f -2 -d.)
+  if [ "$VERSION_PATCH" == "" ] || [ "$VERSION_MAJMIN" == "" ]; then
+    echo npm version failed; bailing out
     exit 0
   fi
 
-  CUR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  GITSTATUS=$(git status -s)
-  GITHASHLOCAL=$(git rev-parse HEAD)
-  GITHASHREMOTE=$(git rev-parse origin/$CUR_BRANCH)
+  echo "Version bumped to" $VERSION_PATCH
 
-  if [ ! -z "$GITSTATUS" ] || [ "$GITHASHLOCAL" != "$GITHASHREMOTE" ]; then
-    echo "You have uncommitted changes; bailing out."
-    exit 0
+  if [ "$versionType" != "patch" ]; then
+    # maj.min branches have no 'v' prefix
+    git branch -m $VERSION_MAJMIN
   fi
 
-  if [ "$CUR_BRANCH" != "master" ]; then
-    echo "Deploying from branch $CUR_BRANCH"
-    read -p "Type $CUR_BRANCH to continue deploying from a custom branch, or Ctrl-C to cancel: " p
-    if [ "$p" != "$CUR_BRANCH" ]; then
-      echo "Name doesn't match; bailing out."
-      exit 0
-    fi
+  # push the commit, use -u for major.minor new branch (ignored for existing branches)
+  git push -u origin HEAD
+
+  # push the tag (note tags have a 'v' prefix)
+  git push origin v$VERSION_PATCH
+
+  # if we just made a new branch, let's make a PR, then go back to the branch we started in
+  if [ "$versionType" != "patch" ]; then
+    echo "Creating a pull request. hub will request a username & password the first time only."
+    hub pull-request
+    git checkout $CUR_BRANCH
   fi
 }
 
