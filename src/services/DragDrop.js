Zorroa/zorroa-js-frontend-library

commit 1b8ed8a2b00e02e069b01d659579341308449a10

Load grandchildren when opening a folder, so children toggle icons can be displayed (#108)
* Load grandchildren when opening a folder, so children toggle icons can be displayed

But prevent network requests for already cached folders
Except always request children of empty folders,
so if someone else creates a child, I can see it.

s/getFolderList/renderFolderList/g

* For folder action getFolderChildren, remove callback arg & use redux-thunk the way it was meant

* Wait to show the folder toggle triangle until children are loaded

* Remove unused code from FolderItem

* Folder hover states for DnD

Add a DnD dropzone on folders

This is to deal with DnD on targets that have children
Hovering over children sends the dragleave event

Set app state flag when dragging, so we can toggle folder drop zones

Dropzones have to be hidden when not dragging
otherwise they block pointer events (folder select / toggle)
Making them visible while dragging lets user see them, also enables the drop
target's pointer events (and temporarily blocks folder select / toggle)

src/services/DragDrop.js
@@ -1,17 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { setIsDragging } from '../actions/appActions'
+import { bindActionCreators } from 'redux'
 
 export function DropTarget (type, params) {
   const {dragOver, drop} = params
 
   return function DropTargetFactory (DropComponent) {
     return class DropTargetHOC extends Component {
+
+      state = {dragHover: false}
+
       render () {
         const {props} = this
         const dropParams = {
-          onDragOver: dragOver.bind(this, props, type),
-          onDrop: drop.bind(this, props, type)
+          onDragEnter: (event) => {
+            this.setState({dragHover: true})
+            return false
+          },
+          onDragLeave: (event) => {
+            this.setState({dragHover: false})
+            return false
+          },
+          onDragOver: (event) => {
+            dragOver(props, type, event)
+            event.preventDefault()
+          },
+          onDrop: (event) => {
+            this.setState({dragHover: false})
+            drop.bind(this, props, type)
+            event.preventDefault()
+          }
         }
-        return <DropComponent {...this.props} type={type} dropparams={dropParams} />
+
+        return <DropComponent {...props} dragHover={this.state.dragHover} dropparams={dropParams} />
       }
     }
   }
@@ -21,17 +43,35 @@ export function DragSource (type, params) {
   return function DragSourceFactory (DragComponent) {
     const {dragStart, dragEnd} = params
 
-    return class DragSourceHOC extends Component {
+    class DragSourceHOC extends Component {
+      static propTypes = {
+        // connect props
+        actions: PropTypes.object.isRequired
+      }
+
       render () {
         const {props} = this
+        const {actions} = props
         const dragParams = {
-          onDragStart: dragStart.bind(this, props, type),
-          onDragEnd: dragEnd.bind(this, props, type),
+          onDragStart: (event) => {
+            dragStart(props, type, event)
+            actions.setIsDragging(true)
+          },
+          onDragEnd: (event) => {
+            dragEnd(props, type, event)
+            actions.setIsDragging(false)
+          },
           draggable: true
         }
 
-        return <DragComponent type={type} {...props} dragparams={dragParams} />
+        return <DragComponent {...props} dragparams={dragParams} />
       }
     }
+
+    return connect(state => ({
+      app: state.app
+    }), dispatch => ({
+      actions: bindActionCreators({ setIsDragging }, dispatch)
+    }))(DragSourceHOC)
   }
 }

src/actions/appActions.js
@@ -1,4 +1,4 @@
-import { MODAL, ICONIFY_LEFT_SIDEBAR, ICONIFY_RIGHT_SIDEBAR, TOGGLE_COLLAPSIBLE, METADATA_FIELDS, TABLE_FIELDS, DISPLAY_OPTIONS } from '../constants/actionTypes'
+import { MODAL, ICONIFY_LEFT_SIDEBAR, ICONIFY_RIGHT_SIDEBAR, TOGGLE_COLLAPSIBLE, METADATA_FIELDS, TABLE_FIELDS, DISPLAY_OPTIONS, SET_DRAGGING } from '../constants/actionTypes'
 
 export function updateModal ({title, footer, content}) {
   return {
@@ -39,6 +39,13 @@ export function updateTableFields (fields) {
   return ({
     type: TABLE_FIELDS,
     payload: fields
+  })
+}
+
+export function setIsDragging (isDragging) {
+  return ({
+    type: SET_DRAGGING,
+    payload: isDragging
   })
 }
 

src/actions/folderAction.js
@@ -19,11 +19,11 @@ export function toggleFolder (folderId, isOpen) {
 // the request will be passed the list of children loaded
 export function getFolderChildren (parentId, optOnDoneFn) {
   if (parentId < Folder.ROOT_ID) {             // Catch "fake" folders, if used
-    return dispatch => { if (optOnDoneFn) optOnDoneFn() }
+    return dispatch => { return Promise.resolve() }
   }
   return dispatch => {
     console.log('Load folder ' + parentId)
-    getArchivist().get(`${rootEndpoint}/${parentId}/_children`)
+    return getArchivist().get(`${rootEndpoint}/${parentId}/_children`)
       .then(response => {
         const children = response.data.map(folder => (new Folder(folder)))
         if (optOnDoneFn) optOnDoneFn(children)

src/components/Folders/FolderItem.js
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import Folder from '../../models/Folder'
 
-import { getFolderChildren, selectFolderIds, addAssetIdsToFolderId } from '../../actions/folderAction'
+import { addAssetIdsToFolderId } from '../../actions/folderAction'
 
 // Renders folder children as Collapsible elements.
 
@@ -24,8 +24,8 @@ const target = {
       var selectedAssetIds = new Set(props.selectedAssetIds)
       selectedAssetIds.add(data.id)
       props.actions.addAssetIdsToFolderId([...selectedAssetIds], props.folder.id)
+    }
   }
-}
 }
 
 @DropTarget('FOLDER', target)
@@ -40,40 +40,35 @@ class FolderItem extends Component {
     isSelected: PropTypes.bool.isRequired,
     onToggle: PropTypes.func,
     onSelect: PropTypes.func,
-
-    // state props
-    folders: PropTypes.object.isRequired
+    dragHover: PropTypes.bool.isRequired
   }
 
   render () {
-    const { folder, depth, isOpen, hasChildren, isSelected, onToggle, onSelect, dropparams } = this.props
+    const { folder, depth, isOpen, hasChildren, isSelected, onToggle, onSelect, dropparams, dragHover } = this.props
     const icon = folder.isDyhi() ? 'icon-cube' : 'icon-folder'
 
     return (
-      <div className={classnames('FolderItem', { isOpen, hasChildren, isSelected })}
-           style={{ paddingLeft:`${(depth - 1) * 10}px` }}
-           {...dropparams}>
+      <div className={classnames('FolderItem', { isOpen, hasChildren, isSelected: isSelected || dragHover })}
+           style={{ paddingLeft: `${(depth - 1) * 10}px` }}>
         <div className='FolderItem-toggle'
-             onClick={event => { onToggle(folder); return false }}
-        >
+             onClick={event => { onToggle(folder); return false }}>
           {(hasChildren) ? <i className='FolderItem-toggleArrow icon-triangle-down'/> : null}
         </div>
-        <div className='FolderItem-select'
-            onClick={event => { onSelect(folder); return false }}
-        >
+        <div className={classnames('FolderItem-select', {dragHover})}
+             onClick={event => { onSelect(folder); return false }}>
           <i className={`FolderItem-icon ${icon}`}/>
           <div className='FolderItem-text' key={folder.id}>
             {folder.name}
           </div>
         </div>
+        <div className={classnames('FolderItem-dropzone', {dragHover})} {...dropparams}/>
       </div>
     )
   }
 }
 
 export default connect(state => ({
-  selectedAssetIds: state.assets.selectedIds,
-  folders: state.folders
+  selectedAssetIds: state.assets.selectedIds
 }), dispatch => ({
-  actions: bindActionCreators({ getFolderChildren, selectFolderIds, addAssetIdsToFolderId, dispatch }, dispatch)
+  actions: bindActionCreators({ addAssetIdsToFolderId }, dispatch)
 }))(FolderItem)

src/components/Folders/FolderItem.scss
@@ -8,6 +8,8 @@ $FolderItemHeight : 25px;
   @include flexAlignItemsCenter;
   @include fullWidth;
 
+  position: relative; // fbo .FolderItem-dropzone, who's position: absolute
+
   height: $FolderItemHeight;
 
   &:not(.isOpen) .FolderItem-toggleArrow {
@@ -51,4 +53,20 @@ $FolderItemHeight : 25px;
 .FolderItem-text {
   @include clampText;
   margin-left: 10px;
+}
+
+.FolderItem-dropzone {
+  display: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: none;
+  border: 1px solid $zorroa-gray;
+  border-radius: 2px;
+
+  .App.isDragging & {
+    display: block;
+  }
 }

src/components/Folders/Folders.js
@@ -35,18 +35,33 @@ class Folders extends Component {
 
   loadChildren (id) {
     const { folders } = this.props
-    var maybeLoadGrandchildren = (children) => {
-      // If I've never loaded these children before, do it once now
+
+    this.props.actions.getFolderChildren(id)
+    .then(action => {
+      if (!action || !action.payload || !action.payload.children) return
+
+      const children = action.payload.children
+      let proms = []
+
+      // If we never loaded these children before, do it once now
       // This is to find out whether the children have children, so we can
       // show or hide UI for toggling folders
-      if (children) children.forEach(child => {
-        // only load child's children if we haven't loaded this child before
-        if (!folders.all.get(child.id)) {
-          this.props.actions.getFolderChildren(child.id)
-        }
-      })
-    }
-    this.props.actions.getFolderChildren(id, maybeLoadGrandchildren)
+      // TODO: the grandchildren requests are network-heavy, and
+      // can be removed if/when we have an API call to query folder counts
+      if (children) {
+        children.forEach(child => {
+          // only load child's children if we haven't loaded this child before
+          // or if the child has no children, so empty folders have a chance to update
+          const childFolder = folders.all.get(child.id)
+          if (!childFolder || !childFolder.childIds || !childFolder.childIds.size) {
+            proms.push(this.props.actions.getFolderChildren(child.id))
+          }
+        })
+      }
+
+      Promise.all(proms)
+      .then(() => { console.log(`Folder children all done loading for ${id}`) })
+    })
   }
 
   deleteFolder = () => {
@@ -68,7 +83,7 @@ class Folders extends Component {
     this.props.actions.selectFolderIds(selectedFolderIds)
   }
 
-  getFolderList (folder, depth) {
+  renderFolderList (folder, depth) {
     const { folders } = this.props
     const isOpen = folders.openFolderIds.has(folder.id)
     const isSelected = folders.selectedFolderIds.has(folder.id)
@@ -78,7 +93,7 @@ class Folders extends Component {
     let folderList = (depth > 0) ? [
       (<FolderItem {...{depth, folder, isOpen, isSelected}}
         key={folder.id}
-        hasChildren={!childIds || childIds.size > 0 /* pretend there are children when we have no information */}
+        hasChildren={childIds && childIds.size > 0}
         onToggle={this.toggleFolder.bind(this, folder)}
         onSelect={this.selectFolder.bind(this, folder)}
       />)
@@ -92,7 +107,7 @@ class Folders extends Component {
         children = children.filter(Folder.Filters[this.props.filterName])
       }
       children.forEach(child => {
-        folderList = folderList.concat(this.getFolderList(child, depth + 1))
+        folderList = folderList.concat(this.renderFolderList(child, depth + 1))
       })
     }
 
@@ -125,7 +140,7 @@ class Folders extends Component {
     const isFolderSelected = selectedFolderIds && selectedFolderIds.size > 0
     const isDisabled = !selectedFolderIds || selectedFolderIds.size !== 1
     const rootFolder = folders.all.get(Folder.ROOT_ID)
-    let folderList = this.getFolderList(rootFolder, 0)
+    let folderList = this.renderFolderList(rootFolder, 0)
 
     return (
       <div className='Folders'>

src/components/Thumb/Thumb.js
@@ -6,7 +6,7 @@ import classnames from 'classnames'
 
 const source = {
   dragStart (props, type, se) {
-    se.dataTransfer.setData('text/plain', JSON.stringify({type, id:props.asset.id}))
+    se.dataTransfer.setData('text/plain', JSON.stringify({type, id: props.asset.id}))
   },
   dragEnd (props, type, se) {
     se.preventDefault()

src/components/Workspace/Workspace.js
@@ -2,13 +2,14 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as assert from 'assert'
+import classnames from 'classnames'
 
-import Header from '../../components/Header'
-import Sidebar from '../../components/Sidebar'
-import Assets from '../../components/Assets'
-import Folders from '../../components/Folders'
-import Racetrack from '../../components/Racetrack'
-import Metadata from '../../components/Metadata'
+import Header from '../Header'
+import Sidebar from '../Sidebar'
+import Assets from '../Assets'
+import Folders from '../Folders'
+import Racetrack from '../Racetrack'
+import Metadata from '../Metadata'
 import Collapsible from '../Collapsible'
 
 import { iconifyLeftSidebar, iconifyRightSidebar, toggleCollapsible, displayOptions, METADATA_DISPLAY_OPTIONS } from '../../actions/appActions'
@@ -95,7 +96,7 @@ class Workspace extends Component {
     })
 
     return (
-      <div className="app">
+      <div className={classnames('App', {isDragging: app.isDragging})}>
         <Header/>
         <div className="Workspace flexRow fullWidth fullHeight">
 

commit 4504d7a9c073b80faefb22adc85f5f2e29489d06

Fix a syntax error I introduced in PR #108

src/services/DragDrop.js
@@ -28,7 +28,7 @@ export function DropTarget (type, params) {
           },
           onDrop: (event) => {
             this.setState({dragHover: false})
-            drop.bind(this, props, type)
+            drop(props, type, event)
             event.preventDefault()
           }
         }