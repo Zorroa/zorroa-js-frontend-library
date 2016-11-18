10e0cd8c5ddf3559a79d3c2d40dd4ed3e6bd5e23
Dan Wexler <wex@zorroa.com>
2016-11-17 23:51:39 -0800

    Basic pie chart. [Finishes #132396767]

--- src/components/Racetrack/Facet.js
+++ src/components/Racetrack/Facet.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import classnames from 'classnames'
+import { ResponsiveContainer, PieChart, Pie, Cell, Tooltip, Text } from 'recharts'
 
 import WidgetModel from '../../models/Widget'
 import Asset from '../../models/Asset'
@@ -13,9 +14,10 @@ import Widget from './Widget'
 import { unCamelCase } from '../../services/jsUtil'
 import DisplayOptions from '../DisplayOptions'
 
-const BAR_CHART = 'BAR'
-const PIE_CHART = 'PIE'
-const COL_CHART = 'COL'
+const BAR_CHART = 'icon-list'
+const PIE_CHART = 'icon-pie-chart'
+const COL_CHART = 'icon-chart-growth'
+const chartTypes = [ BAR_CHART, PIE_CHART, COL_CHART ]
 
 // Manage a single term facet
 class Facet extends Component {
@@ -65,16 +67,8 @@ class Facet extends Component {
     this.props.actions.removeRacetrackWidgetIds([this.props.id])
   }
 
-  selectGraph (type) {
-    console.log('Selected facet graph type ' + type)
-    switch (type) {
-      case BAR_CHART:
-        break
-      case PIE_CHART:
-        break
-      case COL_CHART:
-        break
-    }
+  selectGraph (chartType) {
+    this.setState({ chartType })
   }
 
   selectTerm (term) {
@@ -89,12 +83,12 @@ class Facet extends Component {
     this.modifySliver(this.state.field, terms)
   }
 
-  selectField (event) {
+  selectField = (event) => {
     this.setState({ showDisplayOptions: true })
     event.stopPropagation()
   }
 
-  updateDisplayOptions (event, state) {
+  updateDisplayOptions = (event, state) => {
     const base = state.checkedNamespaces && state.checkedNamespaces.length && state.checkedNamespaces[0]
     if (base && base.length) {
       const field = base + '.raw'
@@ -104,12 +98,48 @@ class Facet extends Component {
     }
   }
 
-  dismissDisplayOptions () {
+  dismissDisplayOptions = () => {
     this.setState({ showDisplayOptions: false })
   }
 
-  render () {
-    const { isIconified, aggs } = this.props
+  renderPieLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, name }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5
+    const RADIAN = Math.PI / 180
+    const s = radius * Math.cos(-midAngle * RADIAN)
+    const t = radius * Math.sin(-midAngle * RADIAN)
+    const r0 = 0.9
+    const x0 = cx + r0 * s
+    const y0 = cy + r0 * t
+    const r1 = 1.7
+    const x1 = cx + r1 * s
+    const y1 = cy + r1 * t
+    return (
+      <svg>
+        <svg>
+          {
+            percent > 0.05 &&
+            <text x={x0} y={y0} fill="white"
+                  textAnchor={x0 > cx ? 'start' : 'end'}
+                  className="Facet-pie-pct" dominantBaseline="central">
+              {`${(percent * 100).toFixed(0)}%`}
+            </text>
+          }
+          {
+            percent > 0.025 &&
+            <text x={x1} y={y1} fill="black"
+                  textAnchor={x1 > cx ? 'start' : 'end'}
+                  className="Facet-pie-label" dominantBaseline="central">
+              { name }
+            </text>
+          }
+        </svg>
+      </svg>
+    )
+  }
+
+  renderChart () {
+    const { aggs } = this.props
+    const { field, terms, chartType } = this.state
     let maxCount = 0
     let minCount = Number.MAX_SAFE_INTEGER
     // Extract the buckets for this widget from the global query using id
@@ -118,27 +148,13 @@ class Facet extends Component {
       maxCount = Math.max(maxCount, bucket.doc_count)
       minCount = Math.min(minCount, bucket.doc_count)
     })
-    const title = Asset.lastNamespace(unCamelCase(this.state.field))
-    return (
-      <Widget className="Facet"
-              header={(
-                <div className="Facet-header flexRow flexJustifySpaceBetween fullWidth">
-                  <span>Facet: {title}</span>
-                  <div onClick={this.selectField.bind(this)} className="icon-cog"></div>
-                </div>
-              )}
-              isIconified={isIconified}
-              icon='icon-bar-graph'
-              onClose={this.removeFilter.bind(this)}>
-        { this.state.showDisplayOptions && (
-          <DisplayOptions selectedFields={[]}
-                          title="Facet Fields"
-                          singleSelection={true}
-                          onUpdate={this.updateDisplayOptions.bind(this)}
-                          onDismiss={this.dismissDisplayOptions.bind(this)}/>
-        )}
-        <div className="Facet-body flexCol">
-        <div className="Facet-value-table flexOn">
+    const data = buckets.map(bucket => ({name: bucket.key, value: bucket.doc_count}))
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042',
+      '#ce2d3f', '#fc6c2c', ' #a11e77', '#b7df4d', '#1875d1' ]
+    switch (chartType) {
+      case BAR_CHART:
+        return (
+          <div className="Facet-value-table">
             <table>
               <thead>
               <tr>
@@ -148,21 +164,82 @@ class Facet extends Component {
               </thead>
               <tbody>
               { buckets && buckets.map(bucket => (
-                <tr className={classnames('Facet-value-table-row', {selected: this.state.terms.indexOf(bucket.key) >= 0})} key={bucket.key} onClick={this.selectTerm.bind(this, bucket.key)}>
+                <tr className={classnames('Facet-value-table-row',
+                  { selected: terms.indexOf(bucket.key) >= 0 })}
+                    key={bucket.key} onClick={this.selectTerm.bind(this, bucket.key)}>
                   <td>{bucket.key}</td>
                   <td>{bucket.doc_count}</td>
                 </tr>
-              ))}
+              )) }
               </tbody>
             </table>
           </div>
+        )
+
+      case PIE_CHART:
+        return (
+          <div className="Facet-pie-chart">
+            <ResponsiveContainer>
+              <PieChart width={300} height={300}>
+                <Pie innerRadius={40} outerRadius={80} paddingAngle={0}
+                     animationBegin={100}
+                     animationDuration={500}
+                     label={this.renderPieLabel}
+                     data={data} onClick={this.selectTerm.bind(this)}>
+                  { data.map((entry, index) => <Cell key={index} fill={COLORS[index % COLORS.length]}/>) }
+                  <Tooltip/>
+                </Pie>
+                <Text>{field}</Text>
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
+        )
+
+      case COL_CHART:
+        return (
+          <div>Col</div>
+        )
+    }
+  }
+
+  renderChartIcon (type) {
+    const { chartType } = this.state
+    const selected = chartType === type
+    return (
+      <button className={classnames('Facet-icon', type, {selected})}
+              key={type} onClick={this.selectGraph.bind(this, type)} />
+    )
+  }
+
+  render () {
+    const { isIconified } = this.props
+    const { field, minCount, showDisplayOptions } = this.state
+    const title = Asset.lastNamespace(unCamelCase(field))
+    return (
+      <Widget className="Facet"
+              header={(
+                <div className="Facet-header flexRow flexJustifySpaceBetween fullWidth">
+                  <span>Facet: {title}</span>
+                  <div onClick={this.selectField} className="icon-cog"></div>
+                </div>
+              )}
+              isIconified={isIconified}
+              icon='icon-bar-graph'
+              onClose={this.removeFilter.bind(this)}>
+        { showDisplayOptions && (
+          <DisplayOptions selectedFields={[]}
+                          title="Facet Fields"
+                          singleSelection={true}
+                          onUpdate={this.updateDisplayOptions}
+                          onDismiss={this.dismissDisplayOptions}/>
+        )}
+        <div className="Facet-body flexCol">
+          { this.renderChart() }
           <div className="Facet-min-value flexRow flexJustifyCenter">
-            {minCount !== maxCount ? `Search is limited to >${minCount} results per keyword` : '' }
+            {minCount && `Search is limited to >${minCount} results per keyword` }
           </div>
           <div className="Facet-footer flexRow flexJustifyCenter">
-            <button style={{color: this.state.chartType === BAR_CHART ? '#fff' : '#808080', background: this.state.chartType === BAR_CHART ? '#a11d77' : '#fff'}} className="Facet-icon icon-list" onClick={this.selectGraph.bind(this, 1)} />
-            <button style={{color: this.state.chartType === PIE_CHART ? '#fff' : '#808080', background: this.state.chartType === PIE_CHART ? '#a11d77' : '#fff'}} className="Facet-icon icon-pie-chart" onClick={this.selectGraph.bind(this, 2)} />
-            <button style={{color: this.state.chartType === COL_CHART ? '#fff' : '#808080', background: this.state.chartType === COL_CHART ? '#a11d77' : '#fff'}} className="Facet-icon icon-chart-growth" onClick={this.selectGraph.bind(this, 3)} />
+            { chartTypes.map(type => this.renderChartIcon(type)) }
           </div>
         </div>
       </Widget>
