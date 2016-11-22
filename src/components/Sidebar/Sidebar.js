b89b55de3721688b8b84a26f0375ccd5fac0c50f
Dan Wexler <wex@zorroa.com>
2016-11-21 18:38:19 -0800

    Resizable sidebars. [Finishes #134648781] (#124)

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -16,9 +16,30 @@ export default class Sidebar extends Component {
     children: PropTypes.node
   }
 
-  static get defaultProps () {
-    return {
-      isRightEdge: false
+  static defaultProps = {
+    isRightEdge: false
+  }
+
+  state = {
+    startx: 0,
+    startw: 0,
+    width: 340    // Tricky ref stuff needed in componentDidMount to get width
+  }
+
+  startDrag = (event) => {
+    const { width } = this.state
+    this.setState({ startx: event.pageX, startw: width })
+  }
+
+  drag = (event) => {
+    const { isRightEdge } = this.props
+    const { startx, startw } = this.state
+    if (!event.pageX) return
+    const dx = (event.pageX - startx) * (isRightEdge ? -1 : 1)
+    const width = Math.min(1020, Math.max(340, startw + dx))
+    const threshold = 4   // Minimize redraws
+    if (Math.abs(width - this.state.width) > threshold) {
+      this.setState({width})
     }
   }
 
@@ -29,17 +50,21 @@ export default class Sidebar extends Component {
 
   render () {
     const arrow = this.buttonChar()
-    const { isIconified, children, onToggle } = this.props
+    const { isIconified, children, onToggle, isRightEdge } = this.props
+    const { width } = this.state
+    const isOpen = !isIconified
     return (
-      <div className={classnames('Sidebar flexOff flexCol fullHeight', { 'isOpen': !isIconified })}>
-        <div className={classnames('Sidebar-button flexOff', { 'left': !this.props.isRightEdge })}
+      <div style={{width}} className={classnames('Sidebar', { isOpen })}>
+        <div className={classnames('open-close-button', { isRightEdge })}
              onClick={onToggle}
         >
           <label>{arrow}{arrow}</label>
         </div>
-        <div className={'Sidebar-scroll flexOn fullHeight'} >
+        <div className={'scroller'} >
           { children }
         </div>
+        { isOpen && <div draggable={true} onDragStart={this.startDrag} onDrag={this.drag}
+                         className={classnames('resize-thumb', { isRightEdge })} /> }
       </div>
     )
   }
