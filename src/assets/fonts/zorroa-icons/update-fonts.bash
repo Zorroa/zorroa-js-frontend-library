d8042a77d7a9aa227506b3a0645e2b2d3f9f17f9
David Hart <david@zorroa.com>
2016-12-03 10:17:55 -0700

    Add update-fonts to fully automate new versions of zorroa-icons.zip from Amber (#157)
    * Add update-fonts to fully automate new versions of zorroa-icons.zip from Amber
    
    * Go back to original branch after updating fonts
    
    * Fix comments
    
    * Assume less about the path & current branch; add explicit branch flags to hub command; update some comments
    
    * Make update-fonts.bash less error prone
    
    Create & checkout branch before unzipping fonts
    push zorroa-icons to origin:zorroa-icons explicitly
    Force push zorroa-icons branch
    Fix hub PR flag order & merge PR to master (origin/master doesn't fly)
    Force create zorroa-icons branch;
      no reason to error if branch zorroa-icons already exists
    Add a blank line to output so Note is more visible

---
+++ src/assets/fonts/zorroa-icons/update-fonts.bash
@@ -0,0 +1,42 @@
+#!/bin/bash -e # -x
+
+# We need the github cli
+command -v hub >/dev/null 2>&1 || { echo >&2 "Please install hub https://hub.github.com/"; exit 0; }
+
+# Make sure you don't have any staged git files
+GITSTATUS=$(git status -s)
+GITHASHLOCAL=$(git rev-parse HEAD)
+CUR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+GITHASHREMOTE=$(git rev-parse origin/$CUR_BRANCH)
+
+if [ ! -z "$GITSTATUS" ] || [ "$GITHASHLOCAL" != "$GITHASHREMOTE" ]; then
+  echo "You have uncommitted changes; bailing out."
+  exit 0
+fi
+
+# make a new branch
+git fetch
+git branch -f zorroa-icons origin/master
+git checkout zorroa-icons
+
+# We need to be sitting in src/assets/fonts/zorroa-icons (where this script is)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+# Download zorroa-icons.zip
+curl -L 'https://docs.google.com/uc?export=download&id=0B2crMLkmt3O9TU9BWm4xeUlLQVU' > zorroa-icons.zip
+
+# unpack the downloaded file in place, with overwrite
+unzip -o zorroa-icons.zip
+
+# check the contents into git, create a pull request
+git add -- .
+git commit -m "Update zorroa-icons"
+git push -f -u origin zorroa-icons
+hub pull-request -b master -h zorroa-icons -m "Update zorroa-icons"
+
+echo ""
+echo "NOTE: you're now in the zorroa-icons branch."
+echo "Once the pull request is merged, delete the branch"
+echo "To preview the fonts and make sure you have the glyphs you expect:"
+echo "open demo.html (after unzipping the new zip file)"
