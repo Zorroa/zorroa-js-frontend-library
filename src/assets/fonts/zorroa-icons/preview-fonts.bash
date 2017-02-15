66e7bba20147851a98680b61b3811a014f70369d
David Hart <david@zorroa.com>
2017-02-15 10:49:55 -0700

    preview-fonts actually needs to download the font .zip; it's not in git

--- src/assets/fonts/zorroa-icons/preview-fonts.bash
+++ src/assets/fonts/zorroa-icons/preview-fonts.bash
@@ -4,6 +4,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+# Download zorroa-icons.zip. This URL is also in update-fonts.bash
+curl -L 'https://docs.google.com/uc?export=download&id=0B2crMLkmt3O9TU9BWm4xeUlLQVU' > zorroa-icons.zip
+
 # unpack the downloaded file in place, with overwrite
 unzip -o zorroa-icons.zip
 
