5789b88936d08836a4dd8a6ca0a9d197863eb34d
Dan Wexler <wex@zorroa.com>
2017-05-09 16:27:24 -0700

    Save searches to the user folder rather than root

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,13 @@
 export default class User {
-  constructor ({ id, username, email, enabled, firstName, lastName }) {
+  constructor ({ id, username, email, enabled, firstName, lastName, permissionId, homeFolderId }) {
     this.id = id
     this.username = username
     this.email = email
     this.enabled = enabled
     this.firstName = firstName
     this.lastName = lastName
+    this.permissionId = permissionId
+    this.homeFolderId = homeFolderId
   }
 
   get name () { return (this.firstName + ' ' + this.lastName) }
