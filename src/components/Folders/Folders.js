fdcba8dfecaee6d8e97eeb17b4fe02d6af82e3a7
Dan Wexler <wex@zorroa.com>
2017-07-26 15:09:21 -0700

    Update to thicker icons.

--- src/components/Folders/Folders.js
+++ src/components/Folders/Folders.js
@@ -485,7 +485,7 @@ class Folders extends Component {
                                    title={cannotAddError || 'Create a new folder'}
                                    onClick={!cannotAddError && this.addFolder}>
               <span className='icon-collections-add'/>
-              <div>NEW</div>
+              <div className="Folders-controls-add-label">NEW</div>
             </div> }
           </div>
           <div className="Folders-sort-selected">
