fbbaebba976986b61d92d57ef7cf39006d6ada8e
Dan Wexler <wex@zorroa.com>
2017-08-28 10:55:29 -0700

    Initial similar menu. Switching live similarity broken.

--- src/components/Assets/index.js
+++ src/components/Assets/index.js
@@ -3,5 +3,4 @@ import './Assets.scss'
 import './AssetCounter.scss'
 import './Pager.scss'
 import './ThumbSizeSlider.scss'
-import './Editbar.scss'
 export default Assets
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { Link } from 'react-router'
+import classnames from 'classnames'
 
 import User from '../../models/User'
 import Asset from '../../models/Asset'
@@ -10,11 +11,13 @@ import DropdownMenu from '../../components/DropdownMenu'
 import Preferences from '../../components/Preferences'
 import Feedback from '../../components/Feedback'
 import Developer from '../../components/Developer'
-import Editbar from '../Assets/Editbar'
 import AssetCounter from '../Assets/AssetCounter'
 import { showModal } from '../../actions/appActions'
 import { archivistBaseURL, saveUserSettings } from '../../actions/authAction'
 import { selectAssetIds } from '../../actions/assetsAction'
+import { similar } from '../../actions/racetrackAction'
+import { weights } from '../Racetrack/SimilarHash'
+import { equalSets } from '../../services/jsUtil'
 
 class Header extends Component {
   static propTypes = {
@@ -27,6 +30,13 @@ class Header extends Component {
     selectedIds: PropTypes.object,
     totalCount: PropTypes.number,
     loadedCount: PropTypes.number,
+    assetFields: PropTypes.object,
+    similar: PropTypes.shape({
+      field: PropTypes.string,
+      values: PropTypes.arrayOf(PropTypes.string).isRequired,
+      ofsIds: PropTypes.arrayOf(PropTypes.string).isRequired
+    }).isRequired,
+    similarAssets: PropTypes.arrayOf(PropTypes.instanceOf(Asset)),
     userSettings: PropTypes.object.isRequired,
     actions: PropTypes.object.isRequired
   }
@@ -55,12 +65,74 @@ class Header extends Component {
     this.props.actions.selectAssetIds(null)
   }
 
+  sortSimilar = () => {
+    const { actions, selectedIds, similarAssets } = this.props
+    if (!selectedIds || !selectedIds.size) return
+    const selectedAssets = [...selectedIds].map(id => (similarAssets.find(asset => (asset.id === id)))).filter(asset => asset)
+    const values = selectedAssets.map(asset => asset.rawValue(this.props.similar.field))
+    const ofsIds = selectedAssets.map(asset => asset.closestProxy(256, 256).id)
+    const similar = { values, ofsIds, weights: weights(ofsIds) }
+    actions.similar(similar)
+  }
+
+  similarFields = () => {
+    const { assetFields } = this.props
+    if (!assetFields) return []
+    return assetFields.string.filter(field => field.toLowerCase().startsWith('similar'))
+  }
+
+  selectSimilarField = (field) => {
+    const { similarAssets, actions } = this.props
+    const values = similarAssets && similarAssets.map(asset => asset.rawValue(field))
+    const similar = { ...this.props.similar, field, values }
+    actions.similar(similar)
+  }
+
+  renderSimilar = () => {
+    const { selectedIds, similar, similarAssets } = this.props
+
+    const similarFields = this.similarFields()
+
+    const nAssetsSelected = selectedIds ? selectedIds.size : 0
+    const selectedAssets = nAssetsSelected && [...selectedIds].map(id => (similarAssets.find(asset => (asset.id === id)))).filter(asset => asset)
+    const similarHashes = selectedAssets && selectedAssets.map(asset => asset.rawValue(this.props.similar.field))
+    const similarActive = similar.field && similar.field.length > 0 && similar.values && similar.values.length > 0
+    const similarValuesSelected = similarActive && similar.values && similarHashes && equalSets(new Set([...similar.values]), new Set([...similarHashes]))
+
+    // Only enable similar button if selected assets have the right hash
+    const canSortSimilar = similarFields && similarFields.length && selectedIds && selectedIds.size > 0 && similar.field && similar.field.length > 0 && !similarValuesSelected
+    const sortSimilar = canSortSimilar ? this.sortSimilar : null
+
+    return (
+      <div className="Editbar-similar-section">
+        <div className={classnames('Editbar-similar', { 'selected': similarActive, 'disabled': !canSortSimilar })}
+             onClick={sortSimilar} title="Find similar assets">
+          <span className="icon-similarity"/>
+          Similar
+        </div>
+        { similarFields && similarFields.length > 1 && (
+          <DropdownMenu>
+            { similarFields.map(field => (
+              <div className="Editbar-similar-menu-item" onClick={_ => this.selectSimilarField(field) }>
+                <div className={`Editbar-similar-menu-item-selected${similar.field === field ? ' icon-check' : ''}`}/>
+                { field.substr(field.lastIndexOf('.') + 1) }
+              </div>
+            ))}
+          </DropdownMenu>
+        )}
+      </div>
+    )
+  }
+
   render () {
     const { sync, user, isDeveloper, isAdministrator, totalCount, loadedCount, selectedIds, monochrome } = this.props
     const baseURL = archivistBaseURL()
 
     const loader = require('./loader-rolling.svg')
     const syncer = !isDeveloper || sync ? <div className="Header-loading sync"/> : <img className="Header-loading" src={loader}/>
+
+    const nAssetsSelected = selectedIds ? selectedIds.size : 0
+    const disabledSelected = !selectedIds || !selectedIds.size
 
     return (
       <nav className="header flexOff flexCenter fullWidth">
@@ -69,9 +141,13 @@ class Header extends Component {
         <div className="header-asset-counter">
           <AssetCounter total={totalCount} collapsed={0} loaded={loadedCount || 0}/>
         </div>
-        <div className="flexOn"></div>
+        <div className={classnames('Editbar-selected', {disabled: disabledSelected})}>
+          { nAssetsSelected ? `${nAssetsSelected} assets selected` : '' }
+          { nAssetsSelected ? (<div onClick={this.deselectAll} className={classnames('Editbar-cancel', 'icon-cancel-circle', {disabledSelected})}/>) : null }
+        </div>
+        <div className="flexOn"/>
+        { this.renderSimilar() }
         <div className="header-menu-bar fullHeight flexCenter">
-          <Editbar selectedAssetIds={selectedIds} onDeselectAll={this.deselectAll} />
           <div className="header-menu">
             <DropdownMenu label="Help">
               <a href="https://zorroa.com/help-center/" target="_blank" className="header-menu-item" >Tutorials</a>
@@ -103,7 +179,7 @@ class Header extends Component {
         </div>
 
         {/* this is stupid/ugly, but neede to keep sidebar & header logo widths in sync */}
-        <div className='header-padding' style={{width: '22px'}}></div>
+        <div className='header-padding' style={{width: '22px'}}/>
       </nav>
     )
   }
@@ -119,11 +195,15 @@ export default connect(state => ({
   selectedIds: state.assets.selectedIds,
   totalCount: state.assets.totalCount,
   loadedCount: state.assets.loadedCount,
+  assetFields: state.assets && state.assets.fields,
+  similar: state.racetrack.similar,
+  similarAssets: state.assets.similar,
   userSettings: state.app.userSettings
 }), dispatch => ({
   actions: bindActionCreators({
     showModal,
     selectAssetIds,
+    similar,
     saveUserSettings
   }, dispatch)
 }))(Header)
