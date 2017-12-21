28d3de6e8d25559885c6fb42d964e0cf08f4e626
Dan Wexler <wex@zorroa.com>
2017-12-21 15:14:30 -0800

    Update GDrive font file.

--- src/assets/fonts/zorroa-icons/update-fonts.bash
+++ src/assets/fonts/zorroa-icons/update-fonts.bash
@@ -24,7 +24,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
 # Download zorroa-icons.zip. This URL is also in preview-fonts.bash
-curl -L 'https://docs.google.com/uc?export=download&id=0B2crMLkmt3O9TU9BWm4xeUlLQVU' > zorroa-icons.zip
+#curl -L 'https://docs.google.com/uc?export=download&id=0B2crMLkmt3O9TU9BWm4xeUlLQVU' > zorroa-icons.zip
+curl -L 'https://drive.google.com/uc?export=download&id=1JwU3-bC_LgMbGINRs6ICi597xta3F996' > zorroa-icons.zip
 
 # unpack the downloaded file in place, with overwrite
 unzip -o zorroa-icons.zip
