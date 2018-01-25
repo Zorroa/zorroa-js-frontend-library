997ce34045bcb27aee52b3c5e1d27054e8466a85
Dan Wexler <wex@zorroa.com>
2018-01-25 15:02:24 -0800

    Throttle selenium instances on slow machines using CPU frequency.

--- tests/selenium/startNode.sh
+++ tests/selenium/startNode.sh
@@ -55,8 +55,11 @@ fi
 # launch & disown selenium node
 logfile=selenium-node-$(date "+%Y_%m_%d_%Hh_%Mm_%Ss").log
 # find out how much mem on this system. This is mac specific
+CPU_GH=$(sysctl hw.cpufrequency | awk '{print $2/1000000000}')
 MEM_GB=$(sysctl hw.memsize | awk '{print int($2/1023**3)}')
-MAX_INSTS=$(( $MEM_GB / 2 - 1 ))
+MAX_CPU_INSTS=$(( $CPU_GH * $CPU_GH ))
+MAX_MEM_INSTS=$(( $MEM_GB / 2 - 1 ))
+MAX_INSTS=$(( $MAX_CPU_INSTS > $MAX_MEM_INSTS ? $MAX_MEM_INSTS : $MAX_CPU_INSTS ))
 # OPTS string from http://www.software-testing-tutorials-automation.com/2016/04/usage-of-maxsession-in-grid-2-to-set.html
 # OPTS="-browser browserName=firefox,maxInstances=2 -browser browserName=chrome,maxInstances=2"
 OPTS="-browser browserName=chrome,maxInstances=$MAX_INSTS -maxSession $MAX_INSTS -debug true"
