5a7b76fa6c4f60a1b3f986c1d62825db5192a718
octaviaaris <oapayne@gmail.com>
2018-08-15 14:27:19 -0700

    change disabledSortFields to array

--- src/services/disableSort.js
+++ src/services/disableSort.js
@@ -1,6 +1,9 @@
+/* This helper function takes in the field and returns true
+if the field is not sortable */
+
 const analysis = new RegExp('^analysis')
 const proxies = new RegExp('^proxies')
-const disabledSortFields = new Set([
+const disabledSortFields = [
   'location.point',
   'media.attrs.ResolutionUnit',
   'media.content:1.0',
@@ -10,10 +13,12 @@ const disabledSortFields = new Set([
   'zorroa.taxonomy.keywords',
   'zorroa.taxonomy.folderId',
   'zorroa.taxonomy.taxId',
-])
+]
 
 export function disableSort(field) {
   const notSortable =
-    analysis.test(field) || proxies.test(field) || disabledSortFields.has(field)
+    analysis.test(field) ||
+    proxies.test(field) ||
+    disabledSortFields.indexOf(field)
   return notSortable
 }
