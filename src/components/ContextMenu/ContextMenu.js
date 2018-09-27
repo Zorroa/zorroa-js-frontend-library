3cb837a6efff1ef539c319bac437909c2e109716
Kitt Hodsden <kitt@users.noreply.github.com>
2018-09-27 10:33:45 -0700

    EVI-948 Hide context menu when user clicks outside of context menu (#868)
    * EVI-948 Hide displayed context menus when user clicks outside of said context menu
    
    * WIP ContextMenu tests, not working
    
    * Write unit test

--- src/components/ContextMenu/ContextMenu.js
+++ src/components/ContextMenu/ContextMenu.js
@@ -36,6 +36,24 @@ export default class ContextMenu extends Component {
     }).isRequired,
   }
 
+  dismissOnClickOutside = event => {
+    if (!this.doesContextMenuRefNodeContainElement(event.target)) {
+      this.props.onDismissFn()
+    }
+  }
+
+  doesContextMenuRefNodeContainElement(element) {
+    return this.contextMenuRefNode && this.contextMenuRefNode.contains(element)
+  }
+
+  componentDidMount = () => {
+    document.addEventListener('click', this.dismissOnClickOutside)
+  }
+
+  componentWillUnmount = () => {
+    document.removeEventListener('click', this.dismissOnClickOutside)
+  }
+
   // Keep the context menu from running off the bottom of the screen
   constrainContextMenu = ctxMenu => {
     if (!ctxMenu) return
@@ -59,31 +77,36 @@ export default class ContextMenu extends Component {
 
     return (
       <div
-        className="ContextMenu"
-        ref={this.constrainContextMenu}
-        onClick={e => {
-          e.preventDefault()
-          onDismissFn()
-        }}
-        onContextMenu={e => {
-          e.preventDefault()
-          onDismissFn()
-        }}
-        style={{ top: contextMenuPos.y, left: contextMenuPos.x }}>
-        <DropdownMenu>
-          <DropdownGroup>
-            {items.map(item => {
-              const isDisabled = item.disabled(selectedFieldIndex)
-              const onClickCallback = isDisabled ? undefined : item.fn
-              return (
-                <DropdownItem key={item.label} onClick={onClickCallback}>
-                  <DropdownFontIcon icon={item.icon} />
-                  {item.label}
-                </DropdownItem>
-              )
-            })}
-          </DropdownGroup>
-        </DropdownMenu>
+        ref={contextMenuRefNode =>
+          (this.contextMenuRefNode = contextMenuRefNode)
+        }>
+        <div
+          className="ContextMenu"
+          ref={this.constrainContextMenu}
+          onClick={e => {
+            e.preventDefault()
+            onDismissFn()
+          }}
+          onContextMenu={e => {
+            e.preventDefault()
+            onDismissFn()
+          }}
+          style={{ top: contextMenuPos.y, left: contextMenuPos.x }}>
+          <DropdownMenu>
+            <DropdownGroup>
+              {items.map(item => {
+                const isDisabled = item.disabled(selectedFieldIndex)
+                const onClickCallback = isDisabled ? undefined : item.fn
+                return (
+                  <DropdownItem key={item.label} onClick={onClickCallback}>
+                    <DropdownFontIcon icon={item.icon} />
+                    {item.label}
+                  </DropdownItem>
+                )
+              })}
+            </DropdownGroup>
+          </DropdownMenu>
+        </div>
       </div>
     )
   }
