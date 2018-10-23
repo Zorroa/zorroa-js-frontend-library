4584518af679099f2bec2c1b5e1ebfd837c4bbe1
Joshua Davis <jd@issuu.com>
2018-10-23 14:01:12 -0700

    Accomdate release types

--- tools/release.js
+++ tools/release.js
@@ -3,8 +3,21 @@ const {join} = require('path')
 const {readFileSync, writeFileSync} = require('fs')
 
 const git = require("nodegit");
+const program = require('commander');
 const DEVELOPMENT_BRANCH = 'jd/storybook' // TODO change this to master
 const RELEASE_BRANCH = 'jd/release' // TODO change this to master
+const RELEASE_TYPES = ['major', 'minor', 'patch']
+
+program
+  .version('0.0.0')
+  .option('-r, --release-type <release-type>', 'Choose a semantic versioning type: major, minor or patch')
+  .parse(process.argv);
+
+if (!RELEASE_TYPES.includes(program.releaseType)) {
+  console.log(program.releaseType)
+  console.log("A release type must be selected. Adhere to SEMVER conventions listed in the README.md. If you missed that section of the README.md, read it *all* again to avoid problems.")
+  process.exit(1)
+}
 
 console.log(`Updating to latest ${DEVELOPMENT_BRANCH}`)
 execSync(`git checkout ${DEVELOPMENT_BRANCH} && git pull origin ${DEVELOPMENT_BRANCH}`)
@@ -22,7 +35,8 @@ execSync(`git merge ${DEVELOPMENT_BRANCH}`)
 const packageJsonFile = readFileSync('./package.json', 'utf8')
 const packageJson = JSON.parse(packageJsonFile)
 const versions = packageJson.version.split('.').map(number => Number.parseInt(number, 10))
-versions[2] = versions[2] + 1
+const releaseLevel = RELEASE_TYPES.indexOf(program.releaseType)
+versions[releaseLevel] = versions[releaseLevel] + 1
 packageJson.version = versions.join('.')
 console.log(`Update version to ${packageJson.version}`)
 writeFileSync('./package.json', JSON.stringify(packageJson, undefined, 2))
