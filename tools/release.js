ecfb1e868237dbfd35b779b58bff757951488c2f
Joshua Davis <jd@issuu.com>
2018-10-23 13:14:26 -0700

    Checkpoint

--- tools/release.js
+++ tools/release.js
@@ -1,9 +1,10 @@
 const {execSync} = require('child_process')
 const {join} = require('path')
 const git = require("nodegit");
+const DEVELOPMENT_BRANCH = 'jd/storybook' // TODO change this to master
 
-console.log('Updating to latest master')
-execSync('git checkout master && git pull origin master')
+console.log(`Updating to latest ${DEVELOPMENT_BRANCH}`)
+execSync(`git checkout ${DEVELOPMENT_BRANCH} && git pull origin ${DEVELOPMENT_BRANCH}`)
 
 console.log('Run tests')
 execSync('npm run test')
